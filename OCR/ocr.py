8ca0d1ca0cc36f55cb7f68cb3cec2ac4b3a03f6b
eckesser <erickesser@gmail.com>
2023-10-09 16:13:00 -0300

    OCR agora pode ser chamada em outras classes para extrair valores das variais

--- OCR/ocr.py
+++ OCR/ocr.py
@@ -5,31 +5,56 @@ import numpy as np
 from PIL import ImageGrab
 import easyocr
 
-def ocr_from_coords(coords, reader):
-    """
-    Captura a região da tela especificada pelas coordenadas e realiza o OCR usando easyocr.
-    """
-    x1, y1, x2, y2 = map(int, coords.split(", "))
-    screenshot = cv2.cvtColor(np.array(ImageGrab.grab(bbox=(x1, y1, x2, y2))), cv2.COLOR_RGB2BGR)
-    results = reader.readtext(screenshot)
-    texts = [result[1] for result in results]
-    return ' '.join(texts)
-
-def main():
-    # Inicialização do reader do easyocr
-    reader = easyocr.Reader(['en'])
-
-    # Carregando as coordenadas
-    with open("Json/coords.json", "r") as file:
-        data = json.load(file)
-        coords_list = list(data['coordinates'].values())
-
-    while True:
-        for coords_key, coords_value in data['coordinates'].items():
-            text = ocr_from_coords(coords_value, reader)
-            print(f"Texto reconhecido nas coordenadas {coords_key}: {text}")
+class OCR:
+    def __init__(self):
+        self.reader = easyocr.Reader(['en'])
         
-        time.sleep(1)
+        # Loading the coordinates
+        with open("Json/coords.json", "r") as file:
+            self.data = json.load(file)
+            self.coords_list = list(self.data['coordinates'].values())
+        
+    def ocr_from_coords(self, coords):
+        """
+        Captura a região da tela especificada pelas coordenadas e realiza o OCR usando easyocr.
+        """
+        x1, y1, x2, y2 = map(int, coords.split(", "))
+        screenshot = cv2.cvtColor(np.array(ImageGrab.grab(bbox=(x1, y1, x2, y2))), cv2.COLOR_RGB2BGR)
+        results = self.reader.readtext(screenshot)
+        texts = [result[1] for result in results]
+        return ' '.join(texts)
 
-if __name__ == "__main__":
-    main()
+    @staticmethod
+    def div(x):
+        try:
+            if x:
+                valores_separados = str(x).split("/")
+                return round((int(valores_separados[0]) / int(valores_separados[1])) * 100)
+        except ValueError as e:
+            # Você pode adicionar um log aqui se necessário
+            pass
+
+    def process_ocr(self):
+        prayer_x = 0
+        hp_x = 0
+        pet_x = 0
+        
+        while True:
+            for coords_key, coords_value in self.data['coordinates'].items():
+                text = self.ocr_from_coords(coords_value)
+                if(coords_key == "coords_life" ):
+                    hp_x = text
+                if(coords_key == "coords_pray"):
+                    prayer_x = text
+                if(coords_key == "coords_pet_life"):
+                    pet_x = text
+
+                print(self.div(hp_x))
+                print(self.div(prayer_x))
+                print(self.div(pet_x))
+
+            time.sleep(1)
+
+# Use example:
+ocr_processor = OCR()
+ocr_processor.process_ocr()
